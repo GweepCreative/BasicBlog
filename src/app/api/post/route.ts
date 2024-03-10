3b1cf8f66c885fa0fa2e2518017fe5ae25a45f6b
GweepCreative <gweepcreative@gmail.com>
2024-03-10 11:51:22 +0300

    Delete Post

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,7 +1,6 @@
 import Blog from "@/utils/Models/Blog";
 import dbConnect from "@/utils/mongoClient";
 
-
 export async function POST(request: Request) {
   const { slug } = await request.json();
   // console.log(slug)
@@ -11,4 +10,18 @@ export async function POST(request: Request) {
   await dbConnect();
   const res = await Blog.findOne({ _id: slug });
   return new Response(JSON.stringify({ data: res }));
+}
+
+export async function DELETE(request: Request) {
+  const { slug } = await request.json();
+  // console.log(slug)
+  // return new Response(JSON.stringify({ data: "hello" }));
+  if (!slug) return new Response(JSON.stringify({ data: [] }));
+
+  await dbConnect();
+  const res = await Blog.findOne({ _id: slug });
+  if (res) {
+    await Blog.deleteOne({ _id: slug });
+    return new Response(JSON.stringify({ success: true }));
+  } else return new Response(JSON.stringify({ data: "Not Found" }));
 }
